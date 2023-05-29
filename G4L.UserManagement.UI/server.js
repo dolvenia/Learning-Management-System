756f6e8c448adaa4a758081fa09664bbc408dec9
Tshimangadzo <mudzanani27@gmail.com>
2023-05-29 16:09:59 +0200

    added procfile

--- G4L.UserManagement.UI/server.js
+++ G4L.UserManagement.UI/server.js
@@ -5,11 +5,13 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/<name-of-app>"));
+app.use(express.static(__dirname + "/dist/g4l.usermanagement.ui"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/<name-of-app>/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/g4l.usermanagement.ui/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`);
