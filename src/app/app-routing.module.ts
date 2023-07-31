44dfa1e87b8e32ea9f5165b7e7db57eb4c2ac67a
Dolvenia <dolvenia@gmail.com>
2023-07-31 12:46:15 +0200

    created-routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { LoginComponent } from './user-management/login/login.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginGuard } from './user-management/login/guards/login.guard';
+import { ApplicantSuccessComponent } from './user-management/applicant-success/applicant-success.component';
+
+const routes: Routes = [
+
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'applicant-success',
+    component: ApplicantSuccessComponent,
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./master-layout/master-layout.module').then(
+        (m) => m.MasterLayoutModule
+      ),
+    canActivate: [LoginGuard],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+
+export class AppRoutingModule {}
---
+++ src/app/user-management/user-management.module.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MaterialModule } from '../shared/material/material.module';
+import { PipesModule } from '../shared/pipes/pipes.module';
+import { EnrolComponent } from './enrol/enrol.component';
+import { FooterComponent } from './login/footer/footer.component';
+import { IconsComponent } from './login/icons/icons.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserManagementComponent } from './user-management.component';
+import { ApplicantSuccessComponent } from './applicant-success/applicant-success.component';
+
+@NgModule({
+  declarations: [
+    UserManagementComponent,
+    LoginComponent,
+    RegisterComponent,
+    EnrolComponent,
+    IconsComponent,
+    FooterComponent,
+    ApplicantSuccessComponent,
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MdbModalModule,
+    PipesModule,
+    MaterialModule,
+  ],
+})
+export class UserManagementModule {}
